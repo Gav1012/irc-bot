5a28287a817e48ca202f782bf79e99e51f222cce
Gav1012 <gavin.poley@gmail.com>
2024-05-03 23:25:09 -0700

    had to update code to be work for windows, other version was for unix

--- irc.cpp
+++ irc.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <string>
 #include <cstring>
-// #include <sys/socket.h>
-// #include <netinet/in.h>
-// #include <netdb.h>
+#include <WinSock2.h>
 
 int main() {
     std::cout << "Hello World" << std::endl;
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "WSAStartup failed." << std::endl;
+        return 1;
+    }
     std::string server = "irc.freenode.net";
     int port = 1234;
     std::string channel = "#your_channel";
