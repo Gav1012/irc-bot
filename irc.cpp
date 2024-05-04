d9bd0f09b267afc973ae8c66e96514c5d2c9c56b
Gav1012 <gavin.poley@gmail.com>
2024-05-03 23:22:27 -0700

    added setup for irc bot

--- irc.cpp
+++ irc.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <string>
+#include <cstring>
+// #include <sys/socket.h>
+// #include <netinet/in.h>
+// #include <netdb.h>
 
 int main() {
     std::cout << "Hello World" << std::endl;
+    std::string server = "irc.freenode.net";
+    int port = 1234;
+    std::string channel = "#your_channel";
+    std::string botname =  "IRC-SampleBot";
+    char buffer[4096];
+
     return 0;
 }
